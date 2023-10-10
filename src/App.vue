c9c7c68c1124886acb7fa7760a12dd8103bd0f52
unknown <SENA@SGLDFPCATSD260.FormacionSena.RED>
2023-10-10 13:12:36 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,61 @@
 <template>
-  <div class="body">
-
-    <div class="tabladiv">
-      <div class="q-pa-md">
-        <q-markup-table dark class="bg-white">
-          <thead class="bg-primary">
-            <tr class="text-dark1">
-              <th colspan="3">DATOS TICKETS</th>
-            </tr>
-            <tr class="text-dark">
-              <th class="text-center">cedula</th>
-              <th class="text-center">nombre</th>
-              <th class="text-center">telefono</th>
-              <th class="text-center">Acciones</th>
-            </tr>
-          </thead>
-          <tbody class="text-dark">
-            <tr v-for="cliente in DatosData" :key="cliente">
-              <td class="text-center">{{ cliente.cedula }}</td>
-              <td class="text-center">{{ cliente.nombre }}</td>
-              <td class="text-center">{{ cliente.telefono }}</td>
-              <td class="text-center">
-                <q-btn class="ttp" icon="edit" color="primary"></q-btn>
-                <q-btn class="ttp" icon="delete" color="negative"></q-btn>
-              </td>
-            </tr>
-          </tbody>
-        </q-markup-table>
-        <div class="text-right">
-          <button class="btn" @click="mostrarModal">Agregar</button>
-        </div>
-
-        <q-dialog v-model="mostrarModalAgregar">
-          <q-card>
-            <q-card-section>
-              <h4>Agregar nuevo elemento</h4>
-              <div class="form-group">
-                <div class="input-group">
-                  <label for="campo1">Campo 1:</label>
-                  <input id="campo1" class="datos" type="text">
-                </div>
-                <div class="input-group">
-                  <label for="campo2">Campo 2:</label>
-                  <input id="campo2" class="datos" type="text">
-                </div>
-                <div class="input-group">
-                  <label for="campo3">Campo 3:</label>
-                  <input id="campo3" class="datos" type="text">
-                </div>
-                <div class="input-group">
-                  <label for="campo4">Campo 4:</label>
-                  <input id="campo4" class="datos" type="text">
-                </div>
-              </div>
-            </q-card-section>
-            <q-card-actions align="right">
-              <q-btn label="Guardar" color="primary" @click="guardarNuevoElemento" />
-              <q-btn label="Cancelar" color="negative" @click="cerrarModal" />
-            </q-card-actions>
-          </q-card>
-        </q-dialog>
-      </div>
-    </div>
+  <div>
+    <q-table title="Buses" :rows="rows" :columns="columns" row-key="name">
+    <template v-slot:body-cell-botones="props">
+      <q-td :props="props">
+        <q-btn color="primary" label="Editar" @click="EditarBus(props.row._id)" />
+        <q-btn color="negative" label="Borrar" @click="borrarBus(props.row._id)" />
+        <q-btn color="amber" glossy :label="habilitar" @click="InactivarBus(props.row._id)" />
+      </q-td>
+    </template>
+  </q-table>
   </div>
+  
 </template>
 
 <script setup>
-import axios from "axios";
-import { ref, onMounted } from "vue";
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
 
-let DatosData = ref([]);
-let mostrarModalAgregar = ref(false);
+let apiUrl = 'https://transporte-czaa.onrender.com/api/bus/buses';
+let buses = ref([]);
+let rows = ref([]);
 
-function mostrarModal() {
-  mostrarModalAgregar.value = true;
+let fixed = ref(false)
+
+let habilitar = ref('Inactivar')
+
+async function obtenerInfo() {
+  try {
+    const responseBuses = await axios.get(apiUrl);
+    buses.value = responseBuses.data.buses; 
+    rows.value = responseBuses.data.buses; 
+  } catch (error) {
+    console.error('Error al obtener la información de los buses:', error);
+  }
 }
 
-function cerrarModal() {
-  mostrarModalAgregar.value = false;
-}
-
-async function ObtenerDatos() {
-  const response = await axios.get(
-    `https://transporte2.onrender.com/api/cliente/cliente`
-  );
-  const data = response.data;
-
-  data.cliente.forEach((cliente) => {
-    DatosData.value.push({
-      Id: cliente._id,
-      cedula: cliente.cedula,
-      nombre: cliente.nombre,
-      telefono: cliente.telefono,
-    });
-  });
-
-  console.log(data);
-}
-
-onMounted(() => {
-  ObtenerDatos();
-});
+const columns = [
+  { name: 'placa', label: 'Placa', field: 'placa', sortable: true },
+  { name: 'numero_bus', label: 'Número de Bus', field: 'numero_bus', sortable: true },
+  { name: 'cantidad_asientos', label: 'Cantidad de Asientos', field: 'cantidad_asientos' },
+  { name: 'empresa_asignada', label: 'Empresa Asignada', field: 'empresa_asignada' },
+  { name: 'estado', label: 'Estado', field: 'estado', format: (val) => (val ? 'Activo' : 'Inactivo') },
+  { name: 'createAT', label: 'Fecha de Creación', field: 'createAT', sortable: true,},
+  { name: 'botones', label: 'Opciones',
+    field: row => null, 
+    format: (val, row) => {
+      return `
+        <q-btn color="primary" label="Editar" @click="editarBus('${row._id}')" />
+        <q-btn color="negative" label="Borrar" @click="borrarBus('${row._id}')" />
+        <q-btn color="amber" glossy :label="Inactivar"  @click="InactivarBus('${row._id}'/>`;
+    },
+    "sortable": false, 
+  },
+];
+onMounted(()=>{
+  obtenerInfo()
+})
 </script>
 
 
