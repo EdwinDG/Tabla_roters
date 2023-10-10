3602dda8b9fc34234707b0acf4eda53bcb716df6
EdwinDG <edwin2004dg@gmail.com>
2023-10-09 21:42:33 -0500

    EDG1

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,66 @@
 <template>
   <div class="body">
-    <div class="container">
-      <button style="margin: 20px 0px" id="boton">Agregar cliente</button>
-    </div>
 
     <div class="tabladiv">
-      
-        <div class="q-pa-md">
-          <q-markup-table dark class="bg-white " >
-            <thead class="bg-primary" >
-              <tr class="text-dark">
-             
-                <th class="text-right ">cedula</th>
-                <th class="text-right">nombre</th>
-                <th class="text-right">telefono</th>
-               
-              </tr>
-            </thead>
-            <tbody class="text-dark">
-              <tr v-for="cliente in DatosData" :key="cliente">
-
-                <td class="text-right">{{cliente.cedula}}</td>
-                <td class="text-right">{{cliente.nombre}}</td>
-                <td class="text-right">{{cliente.telefono}}</td>
-              </tr>
-              
-            </tbody>
-          </q-markup-table>
+      <div class="q-pa-md">
+        <q-markup-table dark class="bg-white">
+          <thead class="bg-primary">
+            <tr class="text-dark1">
+              <th colspan="3">DATOS TICKETS</th>
+            </tr>
+            <tr class="text-dark">
+              <th class="text-center">cedula</th>
+              <th class="text-center">nombre</th>
+              <th class="text-center">telefono</th>
+              <th class="text-center">Acciones</th>
+            </tr>
+          </thead>
+          <tbody class="text-dark">
+            <tr v-for="cliente in DatosData" :key="cliente">
+              <td class="text-center">{{ cliente.cedula }}</td>
+              <td class="text-center">{{ cliente.nombre }}</td>
+              <td class="text-center">{{ cliente.telefono }}</td>
+              <td class="text-center">
+                <q-btn class="ttp" icon="edit" color="primary"></q-btn>
+                <q-btn class="ttp" icon="delete" color="negative"></q-btn>
+              </td>
+            </tr>
+          </tbody>
+        </q-markup-table>
+        <div class="text-right">
+          <button class="btn" @click="mostrarModal">Agregar</button>
         </div>
-      
+
+        <q-dialog v-model="mostrarModalAgregar">
+          <q-card>
+            <q-card-section>
+              <h4>Agregar nuevo elemento</h4>
+              <div class="form-group">
+                <div class="input-group">
+                  <label for="campo1">Campo 1:</label>
+                  <input id="campo1" class="datos" type="text">
+                </div>
+                <div class="input-group">
+                  <label for="campo2">Campo 2:</label>
+                  <input id="campo2" class="datos" type="text">
+                </div>
+                <div class="input-group">
+                  <label for="campo3">Campo 3:</label>
+                  <input id="campo3" class="datos" type="text">
+                </div>
+                <div class="input-group">
+                  <label for="campo4">Campo 4:</label>
+                  <input id="campo4" class="datos" type="text">
+                </div>
+              </div>
+            </q-card-section>
+            <q-card-actions align="right">
+              <q-btn label="Guardar" color="primary" @click="guardarNuevoElemento" />
+              <q-btn label="Cancelar" color="negative" @click="cerrarModal" />
+            </q-card-actions>
+          </q-card>
+        </q-dialog>
+      </div>
     </div>
   </div>
 </template>
@@ -36,21 +68,18 @@
 <script setup>
 import axios from "axios";
 import { ref, onMounted } from "vue";
-const Codigo = ref("");
-const Nombre = ref("");
-const Cantidad = ref("");
-const Precio = ref("");
-const Costo = ref("");
 
-let codigodivRef = ref();
-let nombredivRef = ref();
-let cantidaddivRef = ref();
-let preciodivRef = ref();
-let costodivRef = ref();
-
-let modalbody = ref("");
-let Err = ref("Datos de la tabla");
 let DatosData = ref([]);
+let mostrarModalAgregar = ref(false);
+
+function mostrarModal() {
+  mostrarModalAgregar.value = true;
+}
+
+function cerrarModal() {
+  mostrarModalAgregar.value = false;
+}
+
 async function ObtenerDatos() {
   const response = await axios.get(
     `https://transporte2.onrender.com/api/cliente/cliente`
@@ -69,27 +98,6 @@ async function ObtenerDatos() {
   console.log(data);
 }
 
-
-function formatoHora(hora) {
-  const fechaHora = new Date(hora);
-  const opcionesDeFormato = {
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-  };
-
-  return fechaHora.toLocaleTimeString("es-ES", opcionesDeFormato);
-}
-function formatoFecha(fecha) {
-  const fechaHora = new Date(fecha);
-  const opcionesDeFormato = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-
-  return fechaHora.toLocaleDateString("es-ES", opcionesDeFormato);
-}
 onMounted(() => {
   ObtenerDatos();
 });
@@ -97,5 +105,50 @@ onMounted(() => {
 
 
 <style scoped>
+.tabladiv {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
 
+.text-center {
+  border: 1px solid black;
+}
+
+.text-dark1 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+}
+
+.ttp {
+  margin: 5px;
+}
+
+.form-group {
+  display: flex;
+  flex-direction: column;
+}
+
+.input-group {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+label {
+  margin-right: 20px;
+}
+
+.datos {
+  flex: 1;
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.btn {
+  margin-top: 10px;
+}
 </style>
